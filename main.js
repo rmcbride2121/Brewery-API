19644d4aaffd7191d7411139b5b22bbf4a70004d
Aaron Gonzalez <ajgonzalez.93@gmail.com>
2019-05-27 16:08:07 -0500

    brewery API

--- main.js
+++ main.js
@@ -1,100 +1,23 @@
-//require('dotenv').config()
+require('dotenv').config();
+const fetch = require('node-fetch');
 
-const publicKey = "a0bcb75f9238bdf6361b953fd34f830e";
-const privateKey = "e7458c06a6a98005d3055a05e01ac82e74c2a3dfY";
-let ts = new Date().getTime();
-const stringToHash = ts + privateKey + publicKey;
-const hash = md5(stringToHash);
+const baseURL = 'https://sandbox-api.brewerydb.com/v2/';
 
-let url =
-  "http://gateway.marvel.com:443/v1/public/characters/1009215/comics?orderBy=focDate" +
-  "&ts=" +
-  ts +
-  "&apikey=" +
-  publicKey +
-  "&hash=" +
-  hash;
+  fetch(baseURL + 'locations/?key=' + process.env.KEY + '&postalCode') //eventually add user input
+  .then(res => {
+    if(!res.ok) {
+      throw Error(res.statusText)
+    } return res.json()
+  })
+  .then(obj => {
+    const data = obj.data[0];
 
-let newArray = null;
+    const breweryName = data.name;
+    const breweryAddress = data.streetAddress;
+    const breweryPhone = data.phone;
 
-fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    newArray = data.apis;
-    console.log(data);
-    console.log(newArray);
-    console.log(newArray.operations);
-
-    newArray.map(currentValue => {
-      //selects id #characters on the <ul>
-      let characterList = document.getElementById("characters");
-
-      //create <li> element
-      let createLi = document.createElement("li");
-
-      //adds id #listItem to each <li>
-      createLi.id = "listItem";
-
-      //creates an <img> element
-      let image = document.createElement("img");
-
-      //creates <button> element
-      let button = document.createElement("button");
-
-      //creates text for each <button>
-      let buttonText = document.createTextNode("See More");
-
-      //adds text inside <button>
-      button.appendChild(buttonText);
-
-      //adds id #seeMoreButton to each <button>
-      button.id = "seeMoreButton";
-
-      //when <button> is clicked, adds a <p> element inside each <li>
-      let boolean = true;
-      button.addEventListener("click", e => {
-        //prevents button from being clicked more than once
-        if (boolean == false) {
-          return;
-        }
-        boolean = false;
-
-        //creates a <p> element
-        let ageText = document.createElement("p");
-
-        //adds id #moreInfo to each <p>
-        ageText.id = "moreInfo";
-
-        //creates text for each <p> from api
-        let age = document.createTextNode("Age: " + currentValue.dob.age);
-        let br = document.createElement("br");
-        let zip = document.createTextNode(
-          "Zip Code: " + currentValue.location.postcode
-        );
-
-        //adds text to each <p>
-        ageText.appendChild(age);
-        ageText.appendChild(br);
-        ageText.appendChild(zip);
-
-        //adds <p> inside <li>
-        createLi.appendChild(ageText);
-      });
-
-      //adds the image url from api to <img>
-      image.src = currentValue.Image;
-
-      //adds <img> to <li>
-      createLi.appendChild(image);
-
-      //adds names from api inside each <li>
-      createLi.appendChild(
-        document.createTextNode(currentValue.Character.properties.name)
-      );
-      //adds <button> inside <li>
-      createLi.appendChild(button);
-
-      //adds each <li> inside <ul>
-      characterList.append(createLi);
-    });
-  });
+    console.log(breweryName);
+    console.log(breweryAddress);
+    console.log(breweryPhone);
+  })
+  .catch(err => console.log(`Error,  ${err}`))
